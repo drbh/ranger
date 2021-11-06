17acc477a9c09452ab6a629d653acf850a780b20
drbh <david.richard.holtz@gmail.com>
2021-11-05 22:22:27 -0400

    allow chain upserts

--- src/chain.rs
+++ src/chain.rs
@@ -1,6 +1,7 @@
 // use optioncontracts::OptionContract;
 use itertools::Itertools;
 use std::collections::HashMap;
+use std::convert::From;
 
 #[derive(Debug, Clone)]
 pub struct ContractInstrument {
@@ -20,10 +21,21 @@ pub struct Position {
     pub contract: ContractInstrument,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct OptionChain {
     pub option_map: HashMap<String, Position>,
     pub sorted_key_list: Vec<String>,
+}
+
+impl From<&Position> for optioncontracts::OptionContract {
+    fn from(item: &Position) -> Self {
+        optioncontracts::OptionContract {
+            optiontype: item.contract.optiontype.clone(),
+            direction: optioncontracts::Direction::Long,
+            strike: item.contract.strike,
+            price: item.moment.price,
+        }
+    }
 }
 
 impl OptionChain {
@@ -31,6 +43,57 @@ impl OptionChain {
         Self {
             option_map: HashMap::new(),
             sorted_key_list: vec![],
+        }
+    }
+
+    // upsert option in chain
+    pub fn upsert(&mut self, input: Position) {
+        // 10::Long
+        let key = format!("{}", input.contract.strike);
+
+        match self.sorted_key_list.binary_search_by(|strike| {
+            strike
+                .parse::<f64>()
+                .unwrap_or_default()
+                .partial_cmp(&input.contract.strike)
+                .unwrap_or(std::cmp::Ordering::Less)
+        }) {
+            Ok(_pos) => {
+                // should always be true since we have item in our map
+                if let Some(position) = self.option_map.get_mut(&key) {
+                    *position = input;
+                }
+            }
+            Err(pos) => {
+                // we should insert into map
+                self.sorted_key_list.insert(pos, key.clone());
+
+                // add option to map
+                self.option_map.insert(key, input);
+            }
+        }
+    }
+    // replaces option in chain
+    pub fn replace(&mut self, input: Position) {
+        // 10::Long
+        let key = format!("{}", input.contract.strike);
+
+        match self.sorted_key_list.binary_search_by(|strike| {
+            strike
+                .parse::<f64>()
+                .unwrap_or_default()
+                .partial_cmp(&input.contract.strike)
+                .unwrap_or(std::cmp::Ordering::Less)
+        }) {
+            Ok(_pos) => {
+                // should always be true since we have item in our map
+                if let Some(position) = self.option_map.get_mut(&key) {
+                    *position = input;
+                }
+            }
+            Err(_pos) => {
+                unreachable!()
+            }
         }
     }
 
@@ -75,14 +138,44 @@ impl OptionChain {
     }
 
     // pub fn get_all_n_items(&self) -> Vec<&Position> {
-    pub fn get_all_n_items(&self, n: usize) -> () {
-        let combinations: Vec<_> = self
-            .sorted_key_list
+    pub fn get_all_n_items(&self, n: usize) -> Vec<Vec<&str>> {
+        self.sorted_key_list
             .iter()
             .map(|s| &**s)
             .combinations(n)
-            .collect();
+            .collect()
+    }
 
-        println!("{:#?}", combinations);
+    // pub fn get_all_n_items(&self) -> Vec<&Position> {
+    pub fn get_all_n_options(&self, n: usize) -> Vec<Vec<&Position>> {
+        self.sorted_key_list
+            .iter()
+            .map(|s| &**s)
+            .combinations(n)
+            .map(|key| {
+                key.iter()
+                    .map(|x| self.option_map.get(x.clone()))
+                    .filter_map(|e| e)
+                    .collect::<Vec<&Position>>()
+            })
+            .collect()
+    }
+
+    pub fn get_all_n_options_contract(
+        &self,
+        n: usize,
+    ) -> Vec<Vec<optioncontracts::OptionContract>> {
+        self.sorted_key_list
+            .iter()
+            .map(|s| &**s)
+            .combinations(n)
+            .map(|key| {
+                key.iter()
+                    .map(|x| self.option_map.get(x.clone()))
+                    .filter_map(|e| e)
+                    .map(|y| optioncontracts::OptionContract::from(y))
+                    .collect::<Vec<optioncontracts::OptionContract>>()
+            })
+            .collect()
     }
 }
--- tests/chain_unit.rs
+++ tests/chain_unit.rs
@@ -7,11 +7,14 @@ mod tests {
     use ranger::chain::OptionChain;
     use ranger::chain::Position;
 
+    use ranger::utils;
+
     #[test]
     fn new_chain() {
         let mut call_option_chain = OptionChain::default();
 
-        for strike in 1..5 {
+        // build some silly chain
+        for strike in 1..6 {
             call_option_chain.add(Position {
                 moment: Moment {
                     timestamp: 1,
@@ -24,31 +27,123 @@ mod tests {
             });
         }
 
-        // let mut put_option_chain = OptionChain::new();
+        let _call_sorted = call_option_chain.sorted_elements();
 
-        // for strike in 1..5 {
-        //     put_option_chain.add(Position {
-        //         moment: Moment {
-        //             timestamp: 1,
-        //             price: 1.0,
-        //         },
-        //         contract: ContractInstrument {
-        //             optiontype: optioncontracts::Type::Put,
-        //             strike: (strike * 5) as f64,
-        //         },
-        //     });
-        // }
+        // this builds all 3 option combination
+        let combinations = call_option_chain.get_all_n_options_contract(3);
 
-        let call_sorted = call_option_chain.sorted_elements();
-        // let put_sorted = put_option_chain.sorted_elements();
+        for opts in combinations.iter() {
+            let mut contracts = opts.clone();
 
-        println!("{:#?}", call_option_chain);
+            // duplicate the middle option (make a butterfly)
+            contracts[1].direction = optioncontracts::Direction::Short;
+            contracts.insert(1, contracts[1].clone());
 
-        // call_option_chain.get_all_n_items(2);
-        call_option_chain.get_all_n_items(3);
-        // call_option_chain.get_all_n_items(4);
-        // println!("{:#?}", put_sorted);
+            // tell us whats happening
+            println!("{}", "");
+            println!(
+                "Strikes: {} {} {}",
+                contracts[0].strike, contracts[1].strike, contracts[3].strike,
+            );
+
+            // calc at current price
+            let current_price = 20.0;
+            let total: f64 = contracts
+                .iter()
+                .map(|contract| optioncontracts::execute_option(&contract, current_price))
+                .sum();
+            println!("@ $20: {:?}", total);
+
+            // now build as points and find zeros
+            let contract_values = utils::calculate_contract_values(contracts);
+            let lines_through_zero = utils::get_lines_through_zero(contract_values);
+            let zero_intersections: Vec<f64> = utils::get_zero_intersections(lines_through_zero)
+                .iter()
+                .map(|x| x.0)
+                .collect();
+
+            println!("Intersections: {:?}", zero_intersections);
+            println!("{}", "");
+        }
 
         assert_eq!(1, 0);
     }
+
+    // #[test]
+    // fn new_chain_with_update() {
+    //     let mut call_option_chain = OptionChain::default();
+
+    //     // build some silly chain
+    //     for strike in 1..6 {
+    //         call_option_chain.add(Position {
+    //             moment: Moment {
+    //                 timestamp: 1,
+    //                 price: 1.0,
+    //             },
+    //             contract: ContractInstrument {
+    //                 optiontype: optioncontracts::Type::Call,
+    //                 strike: (strike * 5) as f64,
+    //             },
+    //         });
+    //     }
+
+    //     // let mut put_option_chain = OptionChain::new();
+
+    //     // for strike in 1..5 {
+    //     //     put_option_chain.add(Position {
+    //     //         moment: Moment {
+    //     //             timestamp: 1,
+    //     //             price: 1.0,
+    //     //         },
+    //     //         contract: ContractInstrument {
+    //     //             optiontype: optioncontracts::Type::Put,
+    //     //             strike: (strike * 5) as f64,
+    //     //         },
+    //     //     });
+    //     // }
+
+    //     let _call_sorted = call_option_chain.sorted_elements();
+    //     // let put_sorted = put_option_chain.sorted_elements();
+
+    //     // this builds all 3 option combination
+    //     let combinations = call_option_chain.get_all_n_options_contract(3);
+
+    //     for opts in combinations.iter() {
+    //         let mut contracts = opts.clone();
+
+    //         // duplicate the middle option (make a butterfly)
+    //         contracts[1].direction = optioncontracts::Direction::Short;
+    //         contracts.insert(1, contracts[1].clone());
+
+    //         // tell us whats happening
+    //         println!("{}", "");
+    //         println!(
+    //             "Strikes: {} {} {}",
+    //             contracts[0].strike, contracts[1].strike, contracts[3].strike,
+    //         );
+
+    //         // calc at current price
+    //         let current_price = 20.0;
+    //         let total: f64 = contracts
+    //             .iter()
+    //             .map(|contract| optioncontracts::execute_option(&contract, current_price))
+    //             .sum();
+    //         println!("@ $20: {:?}", total);
+
+    //         // now build as points and find zeros
+    //         let contract_values = utils::calculate_contract_values(contracts);
+    //         let lines_through_zero = utils::get_lines_through_zero(contract_values);
+    //         let zero_intersections: Vec<f64> = utils::get_zero_intersections(lines_through_zero)
+    //             .iter()
+    //             .map(|x| x.0)
+    //             .collect();
+
+    //         println!("Intersections: {:?}", zero_intersections);
+    //         println!("{}", "");
+    //     }
+    //     // call_option_chain.get_all_n_items(4);
+    //     // println!("{:#?}", put_sorted);
+
+    //     assert_eq!(1, 0);
+    // }
 }
